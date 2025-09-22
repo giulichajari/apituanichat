63520a275164142b3d2abe08e729a1536de2c067
Giuliano Caruso <giuli956@hotmail.com>
2025-09-22 13:06:37 -0300

    DB

--- App/Configs/Database.php
+++ App/Configs/Database.php
@@ -10,34 +10,17 @@ class Database
     private static ?Database $instance = null;
     private PDO $connection;
 
-<<<<<<< HEAD
-    private function __construct() {
-        $host = "127.0.0.1";
-        $dbname = "tuanichatbd";
-        $user = "random";
-=======
     private function __construct()
     {
         $host = "localhost";
         $dbname = "tuanichatbd";
         $user = "tuanichat";
->>>>>>> 7f47dd7b63e977d36ae941def79120a83386e839
         $pass = "Argentina1991!";
  
         try {
             $this->connection = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-<<<<<<< HEAD
-    // Guardar en php-error.log
-error_log("DB CONNECTION ERROR: " . $e->getMessage(), 3, "/var/www/apituanichat/php-error.log");
-
-
-
-    // Opcional: responder JSON para APIs
-    header('Content-Type: application/json');  echo json_encode(['error' => $e->getMessage()]); 
-}
-=======
             // Guardar en php-error.log
             error_log("DB CONNECTION ERROR: " . $e->getMessage());
 
@@ -47,7 +30,6 @@ error_log("DB CONNECTION ERROR: " . $e->getMessage(), 3, "/var/www/apituanichat/
             echo json_encode(['error' => 'Error en la conexión a la base de datos']);
             exit();
         }
->>>>>>> 7f47dd7b63e977d36ae941def79120a83386e839
     }
 
     public static function getInstance(): Database
